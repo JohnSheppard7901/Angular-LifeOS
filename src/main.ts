b5f71f3b124b5738f1c4cf240c3654c60f22e9c9
Frey <john.sheppard7901@gmail.com>
2026-01-30 21:58:05 +0100

    created two components

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
+//bootstrapApplication means starting the application
+
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
