c4b9d7b8d89608a11c54a9323f529afd4631731a
JohnSheppard7901 <john.sheppard7901@gmail.com>
2026-02-03 12:07:58 +0100

    created todo list html templete

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,10 @@
 import { Routes } from '@angular/router';
+import { Todopage } from './features/todopage/todopage';
 //import { Dashbourd} from 'module';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'todos',
+        component: Todopage
+    }
+];
--- src/app/features/todopage/todopage.ts
+++ src/app/features/todopage/todopage.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-todopage',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './todopage.html',
   styleUrl: './todopage.css',
 })
 export class Todopage {
-
+  titel = "Feed the cat";
 }
